1b06a4bf9f36f60a574bc0b2b4e52cfeeecfd3d8
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-05 00:58:05 +0600

    post design from Riasat added

--- src/app.svelte
+++ src/app.svelte
@@ -5,19 +5,17 @@
   import Feed from "./routes/feed.svelte";
   import Login from "./routes/login.svelte";
   import Register from "./routes/register.svelte";
-  import Contact from "./routes/contact.svelte";
 
   const routes: Map<string, string> = new Map([
     ["About", "/about"],
     ["Login", "/login"],
     ["Register", "/register"],
     ["Feed", "/"],
-    // ["Contact Us", "/contact"],
   ]);
 </script>
 
 <Router>
-  <div class="w-screen h-screen bg-slate-900 text-slate-100">
+  <div class="w-full h-screen bg-slate-900 text-slate-100">
     <Nav {routes} />
     <div class="pt-8">
       <Route path="/">
@@ -32,9 +30,6 @@
       <Route path="/register">
         <Register />
       </Route>
-      <!-- <Route path="/contact">
-        <Contact />
-      </Route> -->
     </div>
   </div>
 </Router>
