628f4b37b0a09618eade74d65a746b981ec24885
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-04 19:09:14 +0600

    .

--- src/routes/feed.svelte
+++ src/routes/feed.svelte
@@ -4,7 +4,7 @@
 
 <svelte:head>Showing Posts</svelte:head>
 
-<div>
+<div class="bg-slate-900 p-10">
   <Post poster={"Siam"} post={"what the hell this is?"} />
   <Post poster={"Ashraf"} post={"what am I talking about right now?"} />
   <Post poster={"Ashraf"} post={"what am I talking about right now?"} />
