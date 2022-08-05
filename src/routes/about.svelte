795465cbfefe7aef9eb195e176cb650c38082ebc
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-05 15:56:02 +0600

    about page unfinished, will try installing flowbite-svelte again now

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,7 @@
-<div class="mx-auto my-40 w-1/2">Welcome to Harmony!</div>
+<script>
+  import Advertise from "../lib/advertise.svelte";
+</script>
+
+<div class="mx-auto my-40 w-1/2">
+  <Advertise />
+</div>
