6b74972199682133076ffbb594b17521540070fc
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-15 08:20:40 +0600

    temp commit before trying to add tweened

--- src/lib/pollitem.svelte
+++ src/lib/pollitem.svelte
@@ -2,64 +2,14 @@
   import type { Poll } from "./data/polls";
   import FaIcon from "../lib/faIcon.svelte";
   import { flip } from "svelte/animate";
+  import { slide } from "svelte/transition";
+  import { tweened } from "svelte/motion";
   import { cubicOut } from "svelte/easing";
-  export let transition_axis = "y";
 
-  function slide(
-    node,
-    { delay = 0, duration = 500, easing = cubicOut, axis = transition_axis }
-  ) {
-    console.log("hey transition");
-    const style = getComputedStyle(node);
-    const opacity = +style.opacity;
-    const primary_dimension = axis === "y" ? "height" : "width";
-    const primary_dimension_value = parseFloat(style[primary_dimension]);
-    const secondary_dimensions =
-      axis === "y" ? ["Top", "Bottom"] : ["Left", "Right"];
-    const padding_start_value = parseFloat(
-      style.padding + secondary_dimensions[0]
-    );
-    const padding_end_value = parseFloat(
-      style.padding + secondary_dimensions[1]
-    );
-    const margin_start_value = parseFloat(
-      style.margin + secondary_dimensions[0]
-    );
-    const margin_end_value = parseFloat(style.margin + secondary_dimensions[1]);
-    const border_width_start_value = parseFloat(
-      style[`border${secondary_dimensions[0]}Width`]
-    );
-    const border_width_end_value = parseFloat(
-      style[`border${secondary_dimensions[1]}Width`]
-    );
-    return {
-      delay,
-      duration,
-      easing,
-      css: (t) =>
-        "overflow: hidden;" +
-        `opacity: ${Math.min(t * 20, 1) * opacity};` +
-        `${primary_dimension}: ${t * primary_dimension_value}px;` +
-        `padding-${secondary_dimensions[0].toLowerCase()}: ${
-          t * padding_start_value
-        }px;` +
-        `padding-${secondary_dimensions[1].toLowerCase()}: ${
-          t * padding_end_value
-        }px;` +
-        `margin-${secondary_dimensions[0].toLowerCase()}: ${
-          t * margin_start_value
-        }px;` +
-        `margin-${secondary_dimensions[1].toLowerCase()}: ${
-          t * margin_end_value
-        }px;` +
-        `border-${secondary_dimensions[0].toLowerCase()}-width: ${
-          t * border_width_start_value
-        }px;` +
-        `border-${secondary_dimensions[1].toLowerCase()}-width: ${
-          t * border_width_end_value
-        }px;`,
-    };
-  }
+  const progress = tweened(0, {
+    duration: 400,
+    easing: cubicOut,
+  });
 
   export let poll: Poll;
   //sort the poll options by vote count
@@ -132,7 +82,6 @@
               <div
                 class="h-6 bg-yellow-400 rounded"
                 style="width: {option.width}%"
-                transition:slide={{ axis: "x" }}
               />
             {/if}
           </div>
