8016f65a34ba4767246ec13b9b907715815b5d80
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-25 01:11:59 +0600

    tring to add the top breadcrumb

--- src/routes/archive.svelte
+++ src/routes/archive.svelte
@@ -166,24 +166,106 @@
 
   const params = useParams();
   console.log($params);
+  let first_link: string;
+  let second_link: string;
+  $: {
+    console.log($params.group_name, $params.set_name);
+    if ($params.group_name) {
+      first_link = "/archive/" + $params.group_name;
+    } else first_link = null;
+    if ($params.first_name && $params.set_name) {
+      second_link = "/archive/" + $params.group_name + "/" + $params.set_name;
+    } else second_link = null;
+  }
 </script>
 
-<Router>
-  <div class="bg-slate-900 pt-20 pl-10 pr-3 my-5">
-    <Route>
-      {#each question_groups as question_group}
-        <SingleFolderView name={question_group.group_name} />
-      {/each}
-    </Route>
-    <Route path=":group_name" let:params>
-      {#each get_question_sets(params.group_name) as set}
-        <SingleFolderView name={set} />
-      {/each}
-    </Route>
-    <Route path=":group_name/:set_name" let:params>
-      {#each get_questions(params.set_name) as question}
-        <ShowQuestion {question} />
-      {/each}
-    </Route>
-  </div>
-</Router>
+<div class="bg-slate-900 pt-20">
+  <nav
+    class="flex px-5 mx-3 py-3 mt-10 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+    aria-label="Breadcrumb"
+  >
+    <ol class="inline-flex items-center space-x-1 md:space-x-3">
+      <li class="inline-flex items-center">
+        <Link
+          to="/archive"
+          class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+        >
+          <svg
+            class="w-4 h-4 mr-2"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+            /></svg
+          >
+          Archive
+        </Link>
+      </li>
+      {#if $params.group_name}
+        <li>
+          <div class="flex items-center">
+            <svg
+              class="w-6 h-6 text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              ><path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              /></svg
+            >
+            <Link
+              to={first_link}
+              class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+              >{$params.group_name}</Link
+            >
+          </div>
+        </li>
+      {/if}
+      {#if $params.set_name}
+        <li>
+          <div class="flex items-center">
+            <svg
+              class="w-6 h-6 text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              ><path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              /></svg
+            >
+            <Link
+              to={second_link}
+              class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+              >{$params.set_name}</Link
+            >
+          </div>
+        </li>
+      {/if}
+    </ol>
+  </nav>
+
+  <Router>
+    <div class="bg-slate-900 pt-5 pl-10 pr-3 my-5">
+      <Route>
+        {#each question_groups as question_group}
+          <SingleFolderView name={question_group.group_name} />
+        {/each}
+      </Route>
+      <Route path=":group_name" let:params>
+        {#each get_question_sets(params.group_name) as set}
+          <SingleFolderView name={set} />
+        {/each}
+      </Route>
+      <Route path=":group_name/:set_name" let:params>
+        {#each get_questions(params.set_name) as question}
+          <ShowQuestion {question} />
+        {/each}
+      </Route>
+    </div>
+  </Router>
+</div>
