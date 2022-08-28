347bf27ae4f05fd5ea260e0915e6c9f0696e696c
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-28 07:26:50 +0600

    changed stuff to evade login and also changed the styling of user suggestions a bit

--- src/routes/mygroups.svelte
+++ src/routes/mygroups.svelte
@@ -3,7 +3,8 @@
   import Drawer from "../lib/drawer.svelte";
   import FaIcon from "../lib/faIcon.svelte";
   import { fade } from "svelte/transition";
-  import { loadUserHandles, type UserHandle } from "../lib/data/user";
+  import type { UserHandle } from "../lib/data/user";
+  //  import { loadUserHandles } from "../lib/data/user";
   import Loader from "../lib/loader.svelte";
   import { addGroup } from "../lib/data/groups";
   import { current_user } from "../lib/stores/user";
@@ -21,15 +22,38 @@
 
   let added_users: UserHandle[] = [];
 
-  const load = async () => {
-    try {
-      users = await loadUserHandles();
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const load = async () => {
+  //   try {
+  //     users = await loadUserHandles();
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
-  const loadPromise = load();
+  // const loadPromise = load();
+
+  users = [
+    { id: "1", user_name: "sadat999" },
+    { id: "2", user_name: "risenfromashes" },
+    { id: "3", user_name: "pumpa" },
+    { id: "4", user_name: "siam11651" },
+    { id: "5", user_name: "mahabhu" },
+    { id: "6", user_name: "imanik" },
+    { id: "7", user_name: "ishrak26" },
+    { id: "8", user_name: "mahirlabibdihan" },
+    { id: "9", user_name: "shattik420" },
+    { id: "10", user_name: "msh" },
+    { id: "11", user_name: "brainiac77" },
+    { id: "12", user_name: "thecodeheist" },
+    { id: "13", user_name: "recklessraccoon" },
+    { id: "14", user_name: "mijan_rmn" },
+    { id: "15", user_name: "nahinkhan" },
+    { id: "16", user_name: "nafi10" },
+    { id: "17", user_name: "monke" },
+    { id: "18", user_name: "saffatjabin" },
+    { id: "19", user_name: "nabid" },
+    { id: "20", user_name: "aniksaha" },
+  ];
 
   const add_user = (user: UserHandle) => {
     if (!added_users.includes(user)) {
@@ -105,133 +129,139 @@
   >
     <FaIcon type="regular" icon="plus" className="text-xl" />
   </button>
-  {#await loadPromise}
+  <!-- {#await loadPromise}
     <Loader />
-  {:then}
-    <Drawer transition_axis="-x" bind:open={draweropen}>
-      <!-- drawer component -->
-      <div slot="body">
-        <h5
-          id="drawer-label"
-          class="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
-        >
-          <svg
-            class="w-5 h-5 mr-2"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-              clip-rule="evenodd"
-            /></svg
-          >New group
-        </h5>
-        <form action="#" class="mb-6">
-          <div class="mb-6">
-            <label
-              for="title"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Group Name</label
-            >
-            <input
-              type="text"
-              id="title"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Name"
-              required
-              bind:value={name}
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              for="intro"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >Intro ({maxlength - intro.length} characters remaining)</label
-            >
-            <textarea
-              id="intro"
-              rows="4"
-              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="A few words about the group..."
-              {maxlength}
-              bind:value={intro}
-            />
-          </div>
+  {:then} -->
+  <Drawer transition_axis="-x" bind:open={draweropen}>
+    <!-- drawer component -->
+    <div slot="body">
+      <h5
+        id="drawer-label"
+        class="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
+      >
+        <svg
+          class="w-5 h-5 mr-2"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          ><path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clip-rule="evenodd"
+          /></svg
+        >New group
+      </h5>
+      <form action="#" class="mb-6">
+        <div class="mb-6">
+          <label
+            for="title"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Group Name</label
+          >
+          <input
+            type="text"
+            id="title"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Name"
+            required
+            bind:value={name}
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            for="intro"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Intro ({maxlength - intro.length} characters remaining)</label
+          >
+          <textarea
+            id="intro"
+            rows="4"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="A few words about the group..."
+            {maxlength}
+            bind:value={intro}
+          />
+        </div>
 
-          <form>
-            <div class="my-2">
-              {#each added_users as u}
-                <span
-                  id="badge-dismiss-default"
-                  class="inline-flex items-center py-1 px-2 mr-2 my-1 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-200 dark:text-green-800 font-OpenSans"
-                  out:fade
-                >
-                  {u.user_name}
-                  <button
-                    type="button"
-                    class="rounded-lg inline-flex items-center p-0.5 ml-1 text-sm text-green-400 bg-transparent hover:bg-green-200 hover:text-green-900 dark:hover:bg-green-300 dark:hover:text-green-900"
-                    data-dismiss-target="#badge-dismiss-default"
-                    aria-label="Remove"
-                    on:click={() => remove_user(u)}
-                  >
-                    <svg
-                      aria-hidden="true"
-                      class="w-3 h-3"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      ><path
-                        fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                      /></svg
-                    >
-                    <span class="sr-only">Remove badge</span>
-                  </button>
-                </span>
-              {/each}
-            </div>
-            <input
-              type="text"
-              id="search-input"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search users to add"
-              required
-              bind:value={search_term}
-              bind:this={searchInput}
-            />
-          </form>
-
-          {#if filteredUsers.length > 0}
-            <ul
-              id="autocomplete-items-list"
-              class="rounded-lg bg-slate-600 mt-1"
-            >
-              {#each filteredUsers as user, i}
+        <form>
+          <div class="my-2">
+            {#each added_users as u}
+              <span
+                id="badge-dismiss-default"
+                class="inline-flex items-center py-1 px-2 mr-2 my-1 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-200 dark:text-green-800 font-OpenSans"
+                out:fade
+              >
+                {u.user_name}
                 <button
                   type="button"
-                  class="w-full py-4 px-4 transition-all hover:bg-slate-700 text-left rounded-lg font-OpenSans"
-                  on:click|stopPropagation={() => {
-                    add_user(user);
-                  }}
+                  class="rounded-lg inline-flex items-center p-0.5 ml-1 text-sm text-green-400 bg-transparent hover:bg-green-200 hover:text-green-900 dark:hover:bg-green-300 dark:hover:text-green-900"
+                  data-dismiss-target="#badge-dismiss-default"
+                  aria-label="Remove"
+                  on:click={() => remove_user(u)}
                 >
-                  {@html makeMatchBold(user.user_name)}
+                  <svg
+                    aria-hidden="true"
+                    class="w-3 h-3"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    ><path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    /></svg
+                  >
+                  <span class="sr-only">Remove badge</span>
                 </button>
+              </span>
+            {/each}
+          </div>
+          <input
+            type="text"
+            id="search-input"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search users to add"
+            required
+            bind:value={search_term}
+            bind:this={searchInput}
+          />
+        </form>
+
+        {#if filteredUsers.length > 0}
+          <div
+            id="dropdown"
+            class="z-10 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 w-full"
+          >
+            <ul
+              class="py-1 text-base text-gray-700 dark:text-gray-200 w-full"
+              aria-labelledby="dropdownDefault"
+            >
+              {#each filteredUsers as user, i}
+                <li>
+                  <button
+                    class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white w-full text-left rounded-lg"
+                    on:click|stopPropagation={() => {
+                      add_user(user);
+                    }}
+                  >
+                    {@html makeMatchBold(user.user_name)}
+                  </button>
+                </li>
               {/each}
             </ul>
-          {/if}
+          </div>
+        {/if}
 
-          <button
-            type="submit"
-            class="my-5 text-white justify-center flex items-center bg-blue-700 hover:bg-blue-800 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            on:click={create_group}
-          >
-            Create group</button
-          >
-        </form>
-      </div>
-    </Drawer>
-  {/await}
+        <button
+          type="submit"
+          class="my-5 text-white justify-center flex items-center bg-blue-700 hover:bg-blue-800 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          on:click={create_group}
+        >
+          Create group</button
+        >
+      </form>
+    </div>
+  </Drawer>
+  <!-- {/await} -->
 </div>
