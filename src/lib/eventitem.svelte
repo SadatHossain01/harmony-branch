629c6eb7df16994491139848c7b76924d6fb175d
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-08-31 02:36:02 +0600

    Update eventitem.svelte

--- src/lib/eventitem.svelte
+++ src/lib/eventitem.svelte
@@ -135,7 +135,7 @@
     </div>
     <button
       type="submit"
-      class="text-white justify-center flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-full"
+      class="text-white justify-center flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-full"
       on:click|stopPropagation={() => {
         // do stuffs
         draweropen = false;
